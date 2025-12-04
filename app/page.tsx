934f95d048fba633dda0f836ff23b20c30adb1b0
wonszux <fabian.kanczurski@gmail.com>
2025-12-04 18:02:33 +0100

    initiate

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,26 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [round, setRound] = useState(1);
   const [gameOver, setGameOver] = useState<true | false>(false);
+  const [correct, setCorrect] = useState(0);
+
+  useEffect(() => {
+    const randomSquare = Math.floor(Math.random() * (5 - 1) + 1);
+    setCorrect(randomSquare);
+  });
 
   return (
     <div className="flex flex-col min-h-screen justify-center items-center space-y-3">
       <div className="flex flex-row space-x-3">
-        <div className="bg-green-300 min-w-50 min-h-50 rounded-xl"></div>
-        <div className="bg-blue-300 min-w-50 min-h-50 rounded-lg"></div>
+        <div key={1} className="bg-green-300 w-50 h-50 rounded-xl"></div>
+        <div key={2} className="bg-blue-300 w-50 h-50 rounded-lg"></div>
       </div>
       <div className="flex flex-row space-x-3">
-        <div className="bg-yellow-300 min-w-50 min-h-50 rounded-lg"></div>
-        <div className="bg-red-300 min-w-50 min-h-50 rounded-lg"></div>
+        <div key={3} className="bg-yellow-300 w-50 h-50 rounded-lg"></div>
+        <div key={4} className="bg-red-300 w-50 h-50 rounded-lg"></div>
       </div>
     </div>
   );
