c536e60b6ada4d334339a68c5f7688cdb5ffec0f
wonszux <fabian.kanczurski@gmail.com>
2025-12-05 06:12:10 +0100

    aaaaa

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,25 @@ export default function Home() {
   const [round, setRound] = useState(1);
   const [gameOver, setGameOver] = useState<true | false>(false);
   const [correct, setCorrect] = useState(0);
+  const [sequence, setSequence] = useState([]);
 
-  useEffect(() => {
+  function Generate() {
     const randomSquare = Math.floor(Math.random() * (5 - 1) + 1);
     setCorrect(randomSquare);
-  });
+  }
+
+  function ClickHandler(key: any) {
+    console.log(key);
+  }
 
   return (
     <div className="flex flex-col min-h-screen justify-center items-center space-y-3">
       <div className="flex flex-row space-x-3">
-        <div key={1} className="bg-green-300 w-50 h-50 rounded-xl"></div>
+        <div
+          key={1}
+          onClick={ClickHandler}
+          className="bg-green-300 w-50 h-50 rounded-xl"
+        ></div>
         <div key={2} className="bg-blue-300 w-50 h-50 rounded-lg"></div>
       </div>
       <div className="flex flex-row space-x-3">
