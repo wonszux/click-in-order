7cc48b1e5f77013f18e37ece3667728ac65a319b
wonszux <fabian.kanczurski@gmail.com>
2025-12-05 12:33:11 +0100

    oooopa

--- app/page.tsx
+++ app/page.tsx
@@ -2,70 +2,113 @@
 
 import { useEffect, useState } from "react";
 
-const GREEN = "green-300";
-const BLUE = "blue-300";
-const YELLOW = "yellow-300";
-const RED = "red-300";
-const BLINK = "shadow-";
-
 export default function Home() {
   const [round, setRound] = useState(1);
-  const [gameOver, setGameOver] = useState<true | false>(false);
-  const [sequence, setSequence] = useState([0, 1, 2, 3, 0, 1, 2, 3]);
-  const [squareBlink, setSquareblink] = useState("");
+  const [gameStarted, setGameStarted] = useState(false);
+  const [sequence, setSequence] = useState([0]);
+  const [answer, setAnswer] = useState<number[]>([]);
+  const [blinkIndex, setBlinkIndex] = useState<number | null>(null);
+
+  const getColorClass = (color: string, index: number) => {
+    if (index === blinkIndex) {
+      switch (color) {
+        case "green":
+          return "bg-green-700";
+        case "blue":
+          return "bg-blue-700";
+        case "yellow":
+          return "bg-yellow-700";
+        case "red":
+          return "bg-red-700";
+      }
+    } else {
+      switch (color) {
+        case "green":
+          return "bg-green-300";
+        case "blue":
+          return "bg-blue-300";
+        case "yellow":
+          return "bg-yellow-300";
+        case "red":
+          return "bg-red-300";
+      }
+    }
+  };
 
   const Generate = () => {
     const newRandom = Math.floor(Math.random() * 4);
-    setSequence((prev) => [...prev, newRandom]);
+    setSequence((prevRand) => [...prevRand, newRandom]);
   };
 
   const SquaresComponent = () => {
-    const squares = [GREEN, BLUE, YELLOW, RED];
+    const squares = ["green", "blue", "yellow", "red"];
 
-    const handleClick = (square: string) => {
-      console.log(square);
-      Generate();
+    const handleClick = (round: number, sequence: number[], index: number) => {
+      console.log(index);
+      setAnswer((prevAns) => [...prevAns, index]);
+      GetAnswer(round, sequence);
+      console.log(round);
       console.log(sequence);
     };
 
-    const squareItem = squares.map((square, index) => (
+    const squareItem = squares.map((color, index) => (
       <div
-        onClick={() => handleClick(square)}
+        onClick={() => handleClick(round, sequence, index)}
         key={index}
-        className={"w-50 h-50 rounded-lg bg-" + square}
+        className={`w-50 h-50 rounded-lg ${getColorClass(color, index)}`}
       ></div>
     ));
 
     return squareItem;
   };
 
-  // async function blinking(square: any) {
-  //   const blink = BLINK + square;
-  //   for (let i = 0; i < sequence.length; i++) {
-  //     useEffect(() => {
-  //       setSquareblink(blink);
-  //     });
-  //     setTimeout(() => {
-  //       useEffect(() => {
-  //         setSquareblink("");
-  //       });
-  //     }, 1000);
-  //   }
-  // }
+  const Blinking = () => {
+    sequence.forEach((colorIndex, i) => {
+      setTimeout(() => {
+        setBlinkIndex(colorIndex);
+        setTimeout(() => setBlinkIndex(null), 500);
+      }, i * 800);
+    });
+  };
 
-  // const StartGame = () => {
-  //   blinking;
-  // };
+  const GameRestart = (round: number, sequence: number[]) => {
+    setRound(1);
+    setSequence([0]);
+  };
+
+  const GetAnswer = (round: number, sequence: number[]) => {
+    if (answer.length === round) {
+      if (answer === sequence) {
+        setAnswer([]);
+        setRound(round + 1);
+        Generate();
+        alert("you won this round");
+      } else {
+        GameRestart(round, sequence);
+        alert("you lost, restarting");
+      }
+    }
+  };
+
+  const StartGame = () => {
+    Blinking();
+    GetAnswer(round, sequence);
+    setGameStarted(true);
+  };
 
   const StartButton = () => {
     return (
       <div className="flex flex-row justify-center items-center">
-        <button
-          // onClick={StartGame}
-          className=" bg-purple-300 font-black text-black w-50 h-10 rounded-4xl"
-        >
-          Start the Game
-        </button>
+        {!gameStarted ? (
+          <button
+            onClick={() => StartGame()}
+            className=" bg-purple-300 font-black text-black w-50 h-10 rounded-4xl"
+          >
+            Start the Game
+          </button>
+        ) : (
+          <div>{round}</div>
+        )}
       </div>
     );
   };
